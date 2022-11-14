36e668a86836141d8ebd8f2d3638b5dc6cdb50dc
Rob Prouse <rob@prouse.org>
2022-11-13 21:22:11 -0500

    Run the Chip 8 logo demo

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -11,7 +11,7 @@ const int ScreenHeight = Chip8Emulator.ScreenHeight * PixelSize;
 const int InstructionsPerSecond = 700;
 
 var chip8 = new Chip8Emulator();
-chip8.LoadRom(@"..\..\..\..\roms\Keypad Test [Hap, 2006].ch8");
+chip8.LoadRom(@"..\..\..\..\roms\Chip8 emulator Logo [Garstyciuks].ch8");
 
 unsafe
 {
